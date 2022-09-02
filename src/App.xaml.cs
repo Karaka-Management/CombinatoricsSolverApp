c48702358d318c3f85aa1301ba1cb169f44e9aea
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-09-02 11:51:57 +0200

    init

---
+++ src/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CombinatoricsSolverApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static int minCombinations = 1;
+
+        public static int maxCombinations = 6;
+
+        public static double maxError = 0.005;
+
+        public static int maxSolutions = -1; // -1 = all possible solutions
+    }
+}
